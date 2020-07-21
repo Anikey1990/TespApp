98362b94d82b71173774e2e87885bbca97ea7df5
nikitakopylov <anikey1990@mail.ru>
2020-07-21 22:19:53 +0300

    add starting direction feature

--- app/src/main/java/ru/anikey/tespapp/activities/MainActivity.kt
+++ app/src/main/java/ru/anikey/tespapp/activities/MainActivity.kt
@@ -1,12 +1,33 @@
 package ru.anikey.tespapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.anikey.core_injector.FeatureInjector
 import ru.anikey.tespapp.R
+import ru.anikey.tespapp.app.App
+import ru.terrakok.cicerone.android.support.SupportAppNavigator
 
 class MainActivity : AppCompatActivity() {
+
+    private val navigator = SupportAppNavigator(this, R.id.mainContent)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) FeatureInjector.getDirectionFeature(context = this)
+            .featureDirectionStarter()
+            .startFeature(router = App.INSTANCE.getRouter())
     }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        App.INSTANCE.getNavigationHolder().setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        App.INSTANCE.getNavigationHolder().removeNavigator()
+        super.onPause()
+    }
+
 }
--- app/src/main/java/ru/anikey/tespapp/app/App.kt
+++ app/src/main/java/ru/anikey/tespapp/app/App.kt
@@ -3,11 +3,16 @@ package ru.anikey.tespapp.app
 import android.app.Application
 import android.content.Context
 import ru.anikey.tespapp.di.AppComponent
+import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.NavigatorHolder
+import ru.terrakok.cicerone.Router
 
 class App : Application() {
 
     companion object {
         private lateinit var sContext: Context
+        private lateinit var cicerone: Cicerone<Router>
+        lateinit var INSTANCE: App
 
         fun getAppContext(): Context = sContext
     }
@@ -16,6 +21,8 @@ class App : Application() {
         super.onCreate()
 
         sContext = applicationContext
+        cicerone = Cicerone.create()
+        INSTANCE = this
 
         initInjection()
     }
@@ -23,5 +30,9 @@ class App : Application() {
     private fun initInjection() = AppComponent
         .get()
         .inject(app = this)
+
+    fun getRouter(): Router = cicerone.router
+
+    fun getNavigationHolder(): NavigatorHolder = cicerone.navigatorHolder
 
 }
